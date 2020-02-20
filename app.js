764bfb2c6c1c48a4a34e96bc2872022c1d5747fe
Nick Algarra <algarran24@gmail.com>
2020-02-19 21:10:22 -0600

    deploy heroku

--- app.js
+++ app.js
@@ -1,4 +1,133 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const manager = require('./')
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+const generateHTML = require('./template/generateHTML');
+const generateManager = require('./template/generateManager');
+const generateIntern = require('./template/generateIntern');
+const generateEngineer = require('./template/generateEngineer');
+
+function generateMemberHTML(members) {
+    var HTML = '';
+    members.forEach((URLSearchParams) => {
+        switch (URLSearchParams.position) {
+            case 'Manager':
+                const manager = new Manager(URLSearchParams.name, URLSearchParams.id, URLSearchParams.email, URLSearchParams.info);
+                HTML += generateManager.generateHTML(manager);
+                break;
+            case 'Intern':
+                const intern = new Intern(URLSearchParams.name, URLSearchParams.id, URLSearchParams.email, URLSearchParams.info);
+                HTML += generateIntern.generateHTML(intern);
+                break;
+            case 'Engineer':
+                const engineer = new Engineer(URLSearchParams.name, URLSearchParams.id, URLSearchParams.email, URLSearchParams.info);
+                HTML += generateEngineer.generateHTML(engineer);
+                break;
+        }
+    });
+};
+
+inquirer.registerPrompt()
+
+inquirer.prompt([{
+    type: 'confirm',
+    message: 'Adding A New Team Member?',
+    name: 'members',
+    prompts: [
+        {
+            type: 'list',
+            message: 'Choose The Team Member\'s Position: ',
+            name: 'postion',
+            choices: [
+                'Manager',
+                'Intern',
+                'Engineer',
+            ],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What Is The Team Member\'s Name?',
+            validate(value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'Enter Name Please!';
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter ID number: ',
+            validate(value) {
+                const number = /\d+/;
+                if (number.test(value)) { return true; }
+                return 'Enter ID Number Please!';
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What Is The Team Member\'s Email?',
+            validate(value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'Enter Email Please!';
+            },
+        },
+        {
+            when(response) {
+                if (response.position === 'Manager') {
+                    return true;
+                }
+                return false;
+            },
+            type: 'input',
+            name: 'info',
+            message: 'What Is The Manager\'s Office Number?',
+            validate(value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'Enter Office Number Please!'
+            },
+        },
+        {
+            when(response) {
+                if (response.position === 'Intern') {
+                    return true;
+                }
+                return false;
+            },
+            type: 'input',
+            name: 'info',
+            message: 'Enter the Intern\'s School Name: ',
+            validate(value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'Enter School Name Please!'
+            },
+        },
+        {
+            when(response) {
+                if (response.position === 'Engineer') {
+                    return true;
+                }
+                return false;
+            },
+            type: 'input',
+            name: 'info',
+            message: 'What Is The Engineer\'s GitHub Username?',
+            validate(value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'Enter GitHub Username Please!';
+            },
+        },
+    ],
+}]).then((team) => {
+    let HTML = generateMemberHTML(team.members);
+    HTML = generateMemberHTML.generateHTML(HTML);
+    fs.writeFile('./output/team.html', HTML, (err) => {
+        if(err) {
+            return (err);
+        }
+        console.log('Success! File written to team.html in the output folder');
+    });
+});
